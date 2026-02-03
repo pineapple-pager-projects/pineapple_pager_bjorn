ca548c0c5042e88b8cb6f523a2af28c00250406d
brainphreak <brainphreak@brainphreak.net>
2026-02-03 01:03:48 -0800

    Add CRYPTOGRAPHY_OPENSSL_NO_LEGACY env var to payload.sh

--- payloads/user/reconnaissance/pager_bjorn/payload.sh
+++ payloads/user/reconnaissance/pager_bjorn/payload.sh
@@ -13,6 +13,7 @@ PAYLOAD_DIR="/root/payloads/user/reconnaissance/pager_bjorn"
 #
 export PYTHONPATH="$PAYLOAD_DIR/lib:$PAYLOAD_DIR:$PYTHONPATH"
 export LD_LIBRARY_PATH="$PAYLOAD_DIR:$LD_LIBRARY_PATH"
+export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=1
 
 #
 # Check for Python3 + required modules
